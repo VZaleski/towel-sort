307f8c931a402a158512bab5acf64491a1b54c89
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-10-13 15:49:33 +0300

    fix: empty matrix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-    let array = [];
-    for (let i = 0; i < matrix.length; i++) {
-        if(i % 2 === 0){
-            array = array.concat(matrix[i]);
+    function towelSort (matrix) {
+        let array = [];
+        if( matrix === [] || matrix === [ ]){
+            return matrix;
         } else{
-            array = array.concat(matrix[i].reverse());
+            for (let i = 0; i < matrix.length; i++) {
+                if(i % 2 === 0){
+                    array = array.concat(matrix[i]);
+                } else{
+                    array = array.concat(matrix[i].reverse());
+                }
+            }
+            return array;
         }
     }
-    return array;
 }
